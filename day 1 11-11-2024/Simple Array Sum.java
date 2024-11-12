ebb3fb43b66a243989ef7e5c4f52879a5fb447a1
jayavardhan-10 <jayavardhanboguda@gmail.com>
2024-11-12 23:59:37 +0530

    yes 2

--- day 1 11-11-2024/Simple Array Sum.java
+++ day 1 11-11-2024/Simple Array Sum.java
@@ -10,6 +10,7 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 // hello
+// hello
 public class Solution {
     public static void main(String[] args) throws IOException {
     
